b16d46ed7c185b613af22633987b62989123cf4c
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2023-11-20 11:05:42 +0000

    Add all local changes

--- src/i22_bluesky/stubs/__init__.py
+++ src/i22_bluesky/stubs/__init__.py
@@ -1,3 +1,6 @@
 from .linkam import scan_linkam
+from .save import save_device
+from .load import load_device
+from .send_triggers import send_triggers
 
-__all__ = ["scan_linkam"]
+__all__ = ["scan_linkam", "save_device", "load_device", "send_triggers"]
---
+++ src/i22_bluesky/stubs/send_triggers.py
@@ -0,0 +1,21 @@
+from ophyd_async.core import HardwareTriggeredFlyable
+from i22_bluesky.panda.fly_scanning import RepeatedTrigger
+import bluesky.plan_stubs as bps
+from dls_bluesky_core.stubs.flyables import fly_and_collect
+
+
+def send_triggers(flyer: HardwareTriggeredFlyable, exposure: float, deadtime: float):
+    """Send some triggers to detectors and wait for them to collect."""
+    # Do a single batch to start
+    yield from fly_and_collect(flyer)
+    # Setup for many batches
+    many_batches = RepeatedTrigger(
+        num=(1/ exposure),
+        width=exposure,
+        deadtime=deadtime,
+        repeats=9,
+        period=60,
+    )
+    yield from bps.mv(flyer, many_batches)
+    # Then start flying, collecting roughly every step
+    yield from fly_and_collect(flyer)

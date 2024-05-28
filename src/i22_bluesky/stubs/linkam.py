7fb922e50fda823974ab495cb49c09ba4b21758d
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-05-28 12:16:33 +0100

    Update flyer and detector logic

--- src/i22_bluesky/stubs/linkam.py
+++ src/i22_bluesky/stubs/linkam.py
@@ -1,54 +1,68 @@
+from typing import List
 import bluesky.plan_stubs as bps
 import numpy as np
 from dls_bluesky_core.core import group_uuid, step_to_num
-from dls_bluesky_core.stubs.flyables import fly_and_collect
 from dodal.devices.linkam3 import Linkam3
+
 from ophyd_async.core.flyer import HardwareTriggeredFlyable
+from ophyd_async.plan_stubs import time_resolved_fly_and_collect_with_static_seq_table
+from ophyd_async.core import StandardDetector
 
-from i22_bluesky.panda.fly_scanning import RepeatedTrigger
 
-
-# TODO: Make non-flyer signature to allow non-flying with non-flying device?
 def scan_linkam(
     linkam: Linkam3,
     flyer: HardwareTriggeredFlyable,
+    detectors: List[StandardDetector],
     start: float,
     stop: float,
     step: float,
     rate: float,  # deg C/min
-    exposure: float,
-    deadtime: float,
     num_frames: int,
+    exposure: float,
     fly=False,
 ):
-    one_batch = RepeatedTrigger(num=num_frames, width=exposure, deadtime=deadtime)
+    stream_name = "main"
+    shutter_time = 0.004
     start, stop, num = step_to_num(start, stop, step)
     yield from bps.mv(linkam.ramp_rate, rate)
     if fly:
         # Do a single batch to start
         yield from bps.mv(linkam, start)
-        yield from bps.prepare(flyer, one_batch)
-        yield from fly_and_collect(flyer)
-        # Setup for many batches
-        many_batches = RepeatedTrigger(
-            num=num_frames,
-            width=exposure,
-            deadtime=deadtime,
-            repeats=(num - 1),
-            period=abs((stop - start) / (rate / 60)) / (num - 1),
+        yield from time_resolved_fly_and_collect_with_static_seq_table(
+            stream_name=stream_name,
+            detectors=detectors,
+            flyer=flyer,
+            number_of_frames=num_frames,
+            exposure=exposure,
+            shutter_time=shutter_time,
         )
-        yield from bps.prepare(flyer, many_batches)
+        # Setup for many batches
         # Then start flying, collecting roughly every step
         linkam_group = group_uuid("linkam")
         yield from bps.abs_set(linkam, stop, group=linkam_group, wait=False)
         # Collect constantly
-        yield from fly_and_collect(flyer)
+        yield from time_resolved_fly_and_collect_with_static_seq_table(
+            stream_name=stream_name,
+            detectors=detectors,
+            flyer=flyer,
+            number_of_frames=num_frames,
+            exposure=exposure,
+            shutter_time=0,
+            repeats=(num_frames - 1),
+            period=abs((stop - start) / (rate / 60)) / (num - 1),
+        )
         # Make sure linkam has finished
         yield from bps.wait(group=linkam_group)
     else:
         temps = np.linspace(start, stop, num)
         for temp in temps:
             yield from bps.mv(linkam, temp)
-            yield from bps.prepare(flyer, one_batch)
             # Collect at each step
-            yield from fly_and_collect(flyer)
+            yield from time_resolved_fly_and_collect_with_static_seq_table(
+                stream_name=stream_name,
+                detectors=detectors,
+                flyer=flyer,
+                number_of_frames=num_frames,
+                exposure=exposure,
+                shutter_time=shutter_time,
+            )

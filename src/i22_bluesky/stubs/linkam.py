74fa3b09f8b4556d2b32de36533268a73b089efb
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-05-28 15:45:29 +0100

    Use new split plan stubs from ophyd-async

--- src/i22_bluesky/stubs/linkam.py
+++ src/i22_bluesky/stubs/linkam.py
@@ -6,7 +6,10 @@ from dls_bluesky_core.core import group_uuid, step_to_num
 from dodal.devices.linkam3 import Linkam3
 from ophyd_async.core import StandardDetector
 from ophyd_async.core.flyer import HardwareTriggeredFlyable
-from ophyd_async.plan_stubs import time_resolved_fly_and_collect_with_static_seq_table
+from ophyd_async.plan_stubs import (
+    fly_and_collect,
+    prepare_static_seq_table_flyer_and_detectors_with_same_trigger,
+)
 
 
 def scan_linkam(
@@ -28,28 +31,36 @@ def scan_linkam(
     if fly:
         # Do a single batch to start
         yield from bps.mv(linkam, start)
-        yield from time_resolved_fly_and_collect_with_static_seq_table(
-            stream_name=stream_name,
-            detectors=detectors,
+        yield from prepare_static_seq_table_flyer_and_detectors_with_same_trigger(
             flyer=flyer,
+            detectors=detectors,
             number_of_frames=num_frames,
             exposure=exposure,
             shutter_time=shutter_time,
+        )
+        yield from fly_and_collect(
+            stream_name=stream_name,
+            flyer=flyer,
+            detectors=detectors,
         )
         # Setup for many batches
         # Then start flying, collecting roughly every step
         linkam_group = group_uuid("linkam")
         yield from bps.abs_set(linkam, stop, group=linkam_group, wait=False)
         # Collect constantly
-        yield from time_resolved_fly_and_collect_with_static_seq_table(
-            stream_name=stream_name,
-            detectors=detectors,
+        yield from prepare_static_seq_table_flyer_and_detectors_with_same_trigger(
             flyer=flyer,
+            detectors=detectors,
             number_of_frames=num_frames,
             exposure=exposure,
-            shutter_time=0,
+            shutter_time=shutter_time,
             repeats=(num_frames - 1),
             period=abs((stop - start) / (rate / 60)) / (num - 1),
+        )
+        yield from fly_and_collect(
+            stream_name=stream_name,
+            flyer=flyer,
+            detectors=detectors,
         )
         # Make sure linkam has finished
         yield from bps.wait(group=linkam_group)
@@ -58,11 +69,15 @@ def scan_linkam(
         for temp in temps:
             yield from bps.mv(linkam, temp)
             # Collect at each step
-            yield from time_resolved_fly_and_collect_with_static_seq_table(
-                stream_name=stream_name,
-                detectors=detectors,
+            yield from prepare_static_seq_table_flyer_and_detectors_with_same_trigger(
                 flyer=flyer,
+                detectors=detectors,
                 number_of_frames=num_frames,
                 exposure=exposure,
                 shutter_time=shutter_time,
+            )
+            yield from fly_and_collect(
+                stream_name=stream_name,
+                flyer=flyer,
+                detectors=detectors,
             )

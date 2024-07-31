c56ed841e57928a765eced2eb9358146edce5d22
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-07-31 14:17:05 +0100

    sketch out the test for the pressure cell

--- src/i22_bluesky/plans/__init__.py
+++ src/i22_bluesky/plans/__init__.py
@@ -7,9 +7,12 @@ from .stopflow import (
     stopflow,
     stress_test_stopflow,
 )
+from .test_pressure_cell import test_pressure_cell, make_popping_sound
 
 __all__ = [
     "linkam_plan",
+    "make_popping_sound",
+    "test_pressure_cell",
     "stopflow",
     "check_detectors_for_stopflow",
     "check_stopflow_assembly",

802aff6c88e7580df6e12a407f308f6c8741f72f
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-07-31 14:17:05 +0100

    sketch out the test for the pressure cell

--- src/i22_bluesky/plans/__init__.py
+++ src/i22_bluesky/plans/__init__.py
@@ -7,10 +7,13 @@ from .stopflow import (
     stopflow,
     stress_test_stopflow,
 )
+from .test_pressure_cell import make_popping_sound, test_pressure_cell
 
 __all__ = [
     "linkam_plan",
     "save_linkam",
+    "make_popping_sound",
+    "test_pressure_cell",
     "stopflow",
     "check_detectors_for_stopflow",
     "check_stopflow_assembly",
--- src/i22_bluesky/plans/test_pressure_cell.py
+++ src/i22_bluesky/plans/test_pressure_cell.py
@@ -1,9 +1,14 @@
 
+import bluesky.plan_stubs as bps
+from dodal.common import MsgGenerator, inject
 
 
 from dodal.devices.pressure_cell import PressureCell
 
 
-def test_pressure_cell()->MsgGenerator:
+def test_pressure_cell()-> MsgGenerator:
     yield from {}
 
+
+def make_popping_sound() -> MsgGenerator:
+    yield from bps.mv(pressure_cell, 0)

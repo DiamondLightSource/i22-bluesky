7a3cf48e5a446f1d581ce8607fce60b9946f7ba1
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2024-09-16 14:14:35 +0100

    Refactor Linkam plan to take arbitrary segments

--- src/i22_bluesky/util/settings.py
+++ src/i22_bluesky/util/settings.py
@@ -2,12 +2,10 @@ from functools import lru_cache
 from pathlib import Path
 
 import bluesky.plan_stubs as bps
-from ophyd_async.core import Device
-from ophyd_async.epics.areadetector import (
-    NDAttributeDataType,
-    NDAttributesXML,
-    PilatusDetector,
-)
+from dodal.devices.linkam3 import Linkam3
+from ophyd_async.core import StandardDetector
+from ophyd_async.epics.areadetector import NDAttributeDataType, NDAttributesXML
+from ophyd_async.epics.areadetector.drivers import ADBase
 
 SAVES_ROOT = Path(__file__).parent.parent.parent
 
@@ -16,8 +14,12 @@ def get_device_save_dir(plan_name: str) -> Path:
     return SAVES_ROOT / f"pvs/{plan_name}"
 
 
+def get_ad_xml_dir(plan_name: str) -> Path:
+    return SAVES_ROOT / f"xml/{plan_name}"
+
+
 @lru_cache(maxsize=1)
-def make_stats_sum_xml(path: Path) -> str:
+def _enable_stats_sum_xml() -> NDAttributesXML:
     xml = NDAttributesXML()
     xml.add_param(
         "StatsTotal",
@@ -25,34 +27,49 @@ def make_stats_sum_xml(path: Path) -> str:
         NDAttributeDataType.DOUBLE,
         description="Sum of each detector frame",
     )
-    path.write_text(str(xml))
+    return xml
+
+
+@lru_cache(maxsize=1)
+def _enable_pv_stamping(name: str, pv: str, description: str) -> NDAttributesXML:
+    xml = NDAttributesXML()
+    xml.add_epics_pv(name, pv, description=description)
+    return xml
+
+
+@lru_cache(maxsize=1)
+def path_stats_sum_enabled(base_path: Path) -> str:
+    path = base_path / "stats_sum_stamping.xml"
+    path.write_text(str(_enable_stats_sum_xml()))
     return str(path)
 
 
 @lru_cache(maxsize=1)
-def make_saxs_linkam_stamping_xml(linkam: Device, path: Path):
-    xml = NDAttributesXML()
-    pv = linkam.temp.source.split("://")[1]
-    xml.add_epics_pv(
-        "Temperature",
-        f"{pv}",
-        description="Current linkam temperature",
+def path_temp_stamping_enabled(linkam: Linkam3, base_path: Path):
+    path = base_path / "drv_linkam_stamping.xml"
+    path.write_text(
+        str(
+            _enable_pv_stamping(
+                "Temperature",
+                linkam.temp.source.split("://")[1],
+                "Current linkam temperature",
+            )
+        )
     )
-    path.write_text(str(xml))
     return str(path)
 
 
-def load_saxs_linkam_settings(linkam: Device, saxs: PilatusDetector, path: Path):
+def stamp_temp_pv(linkam: Linkam3, stamped_detector: StandardDetector, base_path: Path):
+    assert isinstance(driver := stamped_detector.drv, ADBase)
     yield from bps.mv(
-        saxs.stats.nd_attributes_file,
-        make_stats_sum_xml(path / "stats_sum_stamping.xml"),
-        saxs.drv.nd_attributes_file,
-        make_saxs_linkam_stamping_xml(linkam, path / "drv_linkam_stamping.xml"),
+        driver.nd_attributes_file,
+        path_temp_stamping_enabled(linkam, base_path),
     )
 
 
-def load_waxs_settings(waxs: PilatusDetector, path: Path):
+def enable_stats_sum(detector: StandardDetector, base_path: Path):
+    assert isinstance(stats := detector.stats, ADBase)
     yield from bps.mv(
-        waxs.stats.nd_attributes_file,
-        make_stats_sum_xml(path / "stats_sum_stamping.xml"),
+        stats.nd_attributes_file,
+        path_stats_sum_enabled(base_path),
     )

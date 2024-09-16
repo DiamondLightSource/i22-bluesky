92632c54093bb7d6f8b14f1b7b950a6078fce655
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2024-09-16 14:14:35 +0100

    Convert to Pydantic 1.0 methods

--- src/i22_bluesky/stubs/linkam.py
+++ src/i22_bluesky/stubs/linkam.py
@@ -1,4 +1,4 @@
-from typing import Self
+from typing import Any
 
 import bluesky.plan_stubs as bps
 import numpy as np
@@ -11,93 +11,98 @@ from ophyd_async.plan_stubs import (
     fly_and_collect,
     prepare_static_seq_table_flyer_and_detectors_with_same_trigger,
 )
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, root_validator
 
 
 class LinkamPathSegment(BaseModel):
     stop: float = Field(
-        "Target final temperature, and initial temperature of next segment.",
+        description="Target final temperature and initial temperature of next segment.",
         json_schema_extra={"units": "°C"},
     )
     rate: float = Field(
-        "Absolute value of temperature change rate |dT/dt|.",
+        description="Absolute value of temperature change rate |dT/dt|.",
         json_schema_extra={"units": "°C/minute"},
         gt=0.0,
     )
     step: float | None = Field(
-        "Temp change to generate points on this segment from. \
+        description="Temp change to generate points on this segment from. \
             Ignored if `num` if set. May be |dT| if stop < start.",
         json_schema_extra={"units": "°C"},
         default=None,
     )
     num: int | None = Field(
-        "Number of equally spaced points to capture along this segment. \
+        description="Number of equally spaced points to capture along this segment. \
             Overrides `step` if set. 1 gives only start of segment.",
         gt=0,
         default=None,
     )
     num_frames: int | None = Field(
-        "Number of frames to capture at each captured point. \
+        description="Number of frames to capture at each captured point. \
             If not set, must be set for overall trajectory.",
         gt=0,
         default=None,
     )
     exposure: float | None = Field(
-        "Exposure time for detector(s) per frame. \
+        description="Exposure time for detector(s) per frame. \
             If not set, must be set for overall trajectory.",
         gt=0.0,
         default=None,
         json_schema_extra={"units": "s"},
     )
     flown: bool = Field(
-        "Whether this segment should be flown (temperature controller begins move, \
-        then frames are captured periodically) or stepped (temperature controller \
-        moves, stops then frames are captured).",
+        description="Whether this segment should be flown (temperature controller \
+            begins move, then frames are captured periodically) or stepped \
+            (temperature controller moves, stops then frames are captured).",
         default=True,
     )
 
-    @model_validator(mode="after")
-    def check_num_or_step_set(self) -> Self:
-        if self.num is None and self.step is None:
+    @root_validator
+    def check_num_or_step_set(cls, values: dict[str, Any]) -> dict[str, Any]:
+        num, step = values.get("num"), values.get("step")
+        if num is None and step is None:
             raise ValueError("Must have set at least one of 'num', 'step'")
-        return self
+        return values
 
 
 class LinkamTrajectory(BaseModel):
     start: float = Field(
-        "Initial temperature of 1st segment.", json_schema_extra={"units": "°C"}
+        description="Initial temperature of 1st segment.",
+        json_schema_extra={"units": "°C"},
     )
     path: list[LinkamPathSegment] = Field(
         description="Ordered list of segments describing the temperature path.",
         min_length=1,
     )
     default_num_frames: int | None = Field(
-        "Number of frames to collect if not overriden by segment. \
+        description="Number of frames to collect if not overriden by segment. \
         Must be set if any segment does not define for itself.",
         gt=0,
         default=None,
     )
     default_exposure: float | None = Field(
-        "Exposure time for each frame if not overriden by segment. \
+        description="Exposure time for each frame if not overriden by segment. \
         Must be set if any segment does not define for itself.",
         gt=0.0,
         default=None,
         json_schema_extra={"units": "s"},
     )
 
-    @model_validator(mode="after")
-    def check_defaults(self) -> Self:
-        if self.default_num_frames is None and any(
-            segment.num_frames is None for segment in self.path
+    @root_validator
+    def check_defaults(cls, values: dict[str, Any]) -> dict[str, Any]:
+        path: list[LinkamPathSegment] = values["path"]
+        default_num_frames: int | None = values["default_num_frames"]
+        default_exposure: float | None = values["default_exposure"]
+        if default_num_frames is None and any(
+            segment.num_frames is None for segment in path
         ):
             raise ValueError(
                 "Number of frames not set for default and for some segment(s)!"
             )
-        if self.default_exposure is None and any(
-            segment.exposure is None for segment in self.path
+        if default_exposure is None and any(
+            segment.exposure is None for segment in path
         ):
             raise ValueError("Exposure not set for default and for some segment(s)!")
-        return self
+        return values
 
 
 def capture_temp(

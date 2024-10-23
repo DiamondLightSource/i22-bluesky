fb419445b9f80c2340708ceb8260e725a9e0c73d
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2024-10-23 14:48:31 +0100

    connect_immediately default false

--- src/i22_bluesky/util/default_devices.py
+++ src/i22_bluesky/util/default_devices.py
@@ -25,29 +25,29 @@ from ophyd_async.core import StandardDetector
 from ophyd_async.fastcs.panda import HDFPanda
 
 FAST_DETECTORS: set[StandardDetector] = {
-    saxs(connect_immediately=False),
-    waxs(connect_immediately=False),
-    i0(connect_immediately=False),
-    it(connect_immediately=False),
+    saxs(),
+    waxs(),
+    i0(),
+    it(),
 }
 
-DETECTORS: set[StandardDetector] = FAST_DETECTORS | {oav(connect_immediately=False)}
+DETECTORS: set[StandardDetector] = FAST_DETECTORS | {oav()}
 
 BASELINE_DEVICES: set[Readable] = {
-    fswitch(connect_immediately=False),
-    slits_1(connect_immediately=False),
-    slits_2(connect_immediately=False),
-    slits_3(connect_immediately=False),
-    slits_4(connect_immediately=False),
-    slits_5(connect_immediately=False),
-    slits_6(connect_immediately=False),
-    hfm(connect_immediately=False),
-    vfm(connect_immediately=False),
-    undulator(connect_immediately=False),
-    dcm(connect_immediately=False),
-    synchrotron(connect_immediately=False),
+    fswitch(),
+    slits_1(),
+    slits_2(),
+    slits_3(),
+    slits_4(),
+    slits_5(),
+    slits_6(),
+    hfm(),
+    vfm(),
+    undulator(),
+    dcm(),
+    synchrotron(),
 }
 
-PANDA: HDFPanda = panda1(connect_immediately=False)
-LINKAM: Linkam3 = linkam(connect_immediately=False)
-STAMPED_DETECTOR: StandardDetector = saxs(connect_immediately=False)
+PANDA: HDFPanda = panda1()
+LINKAM: Linkam3 = linkam()
+STAMPED_DETECTOR: StandardDetector = saxs()

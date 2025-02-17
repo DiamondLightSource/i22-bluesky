fb2399501333fbdb87750f098cd8e071e2cd7634
Will Barnett <william.barnett@diamond.ac.uk>
2025-02-17 13:39:01 +0000

    Updated pressure cell plans following AllValvesControl simplified interface

--- src/i22_bluesky/plans/test_pressure_cell.py
+++ src/i22_bluesky/plans/test_pressure_cell.py
@@ -39,9 +39,9 @@ def lower_pressure(
     """
     # todo not sure what is the difference exactly
     yield from bps.read(
-        pressure_cell.all_valves_control.set_valve(6, FastValveControlRequest.OPEN)
+        pressure_cell.all_valves_control.set({6:FastValveControlRequest.OPEN})
     )
-    yield pressure_cell.all_valves_control.set_valve(6, FastValveControlRequest.OPEN)
+    yield pressure_cell.all_valves_control.set({6:FastValveControlRequest.OPEN})
     # the pressure lowering itself
     yield pressure_cell.pump.pump_motor_direction(PumpMotorDirectionState.REVERSE)
     yield pressure_cell.pump.pump_position.set(target_pressure)

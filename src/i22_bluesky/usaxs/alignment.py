a7753d77d00c814bb3383dd7cf86b412abf1bbd5
Emily Arnold <emily.arnold@diamond.ac.uk>
2025-12-04 18:10:45 +0000

    Add device details, details for alignment scans

--- src/i22_bluesky/usaxs/alignment.py
+++ src/i22_bluesky/usaxs/alignment.py
@@ -1,3 +1,93 @@
+# Common scan types
+def relative_scan() -> None: ...
+
+
+"""Do a relative scan around a point, with backlash correction, return results
+Get starting position of motor
+Do relative scan around that position
+Move back to starting position
+Fit and return results
+"""
+
+
+def knife_edge_scan() -> None: ...
+
+
+"""Do a relative scan and return the position of the edge
+(1st derivative peak)
+"""
+
+
+def peak_scan() -> None: ...
+
+
+"""Do a relative scan and return the position of the peak
+(peak fit to data)
+"""
+
+
+def channel_scan() -> None: ...
+
+
+"""Do a relative scan and return the position of the centre
+(centre of 1st derivative peaks)
+"""
+
+
+def max_val_scan() -> None: ...
+
+
+"""Do a relative scan and return the position maximum value
+"""
+
+
+# Shared Alignment Routines
+def parallel_condition_refiner() -> None: ...
+
+
+"""Iteratively refine position of crystal to ensure it is || to beam
+"""
+
+# number_of_iterations = 0
+# on_peak = False
+# while not on_peak:
+#   Get starting position of yaw motor
+#   Do a max_val_scan of the yaw motor (-num to +num, in steps of num (3 points))
+#   Move to position
+#   Do a knife_edge_scan of x (+/- 0.5 mm, step 0.05 mm)
+#   Move to position
+#   if floor(yawPosition) == floor(startingPosition)):
+#       on_peak = True
+#   if number_of_iterations > 10:
+#       stop refining - you didn't find it
+#   inrement number_of_iterations
+
+
+def align_crystal() -> None: ...
+
+
+"""Do a relative scan and return the position of the edge (1st derivative peak)
+"""
+
+# Do a wide channel_scan of channel (+/- 5 mm, step 0.1 mm)
+# Move to position
+# Do a wide channel_scan of yaw (+/- 6 deg, step 0.3 deg)
+# Move to position
+# Do a knife_edge_scan of rotation face (relative scan 0 mm to 2.5 mm, step 0.05 mm)
+# Move to position
+# Do a parallel_condition_refiner with yaw_value = 5 deg
+# Do a parallel_condition_refiner with yaw_value = 1 deg
+# Do a max_val_scan of the yaw motor (+/- 1.2 deg, step 0.03 deg)
+# Move to position
+# Do a knife_edge_scan of the x motor (+/- 0.5 mm, step 0.01 mm)
+# Move to position
+
+# Get starting position of diode_x, add 12.39 mm and move it there
+# Get the energy you're at
+# Get starting position of yaw, add bragg_angle for energy and move it there
+
+
+# Unique Alignment Routines
 def align_upstream() -> None: ...
 
 
--- src/i22_bluesky/usaxs/setup.py
+++ src/i22_bluesky/usaxs/setup.py
@@ -1,8 +1,8 @@
 # Instantiate motors and encoders (reading fine yaw rotation) in GDA
 # This should go in dodal.
 
-# the diode motore is on a geo-brick
-# the diode motor and the crystor motor need the same acceleration time
+# the diode motor is on a geo-brick
+# the diode motor and the crystal motor need the same acceleration time
 # (Dead reckoning)
 
 # In ScanSpecSeqTableTriggerLogic make GPIO low-> high lines optional.
@@ -12,6 +12,7 @@ from dodal.common.beamlines.beamline_utils import (
 )
 from dodal.devices.motors import Stage, XYStage
 from dodal.utils import BeamlinePrefix, get_beamline_name
+from ophyd_async.core import SingalR
 from ophyd_async.epics.motor import Motor
 
 BL = get_beamline_name("i22")
@@ -19,7 +20,7 @@ PREFIX = BeamlinePrefix(BL)
 
 
 @device_factory()
-def sample_stage() -> XYStage:
+def usaxs_sample_stage() -> XYStage:
     return XYStage(
         prefix=f"{PREFIX.beamline_prefix}-MO-USAXS-01:",
         x_infix="XTRANS:",
@@ -37,15 +38,34 @@ class XYawFineYawStage(Stage):
         name: str = "",
         x_infix: str = _X,
         yaw_infix: str = "YAW",
-        fine_yaw_infix: str = "FINE-YAW",
+        fine_yaw_infix: str = "FINE_YAW",
+        fine_yaw_rbv_infix: str = "FY",
     ):
         with self.add_children_as_readables():
             self.x = Motor(prefix + x_infix)
             self.yaw = Motor(prefix + yaw_infix)
             self.fine_yaw = Motor(prefix + fine_yaw_infix)
+            self.fine_yaw_rbv = SingalR(prefix + fine_yaw_rbv_infix)
         super().__init__(name=name)
 
 
 @device_factory()
+def upstream_crystal_tower() -> XYawFineYawStage:
+    return XYawFineYawStage(
+        prefix=f"{PREFIX.beamline_prefix}-MO-USAXS-01:",
+        x_infix="UPSX",
+        yaw_infix="UPSYAW",
+        fine_yaw_infix="MCS2:YAW1",
+        fine_yaw_rbv_infix="UPSFY",
+    )
+
+
+@device_factory()
 def downstream_crystal_tower() -> XYawFineYawStage:
-    return XYawFineYawStage(prefix=f"{PREFIX.beamline_prefix}-MO-USAXS-01:")
+    return XYawFineYawStage(
+        prefix=f"{PREFIX.beamline_prefix}-MO-USAXS-01:",
+        x_infix="DWNX",
+        yaw_infix="DWNYAW",
+        fine_yaw_infix="MCS2:YAW2",
+        fine_yaw_rbv_infix="DWNFY",
+    )
